c18e24f8b3c2ac065cb95088ec4c45a1ff5d51cf
Muqsit <notmuqsit+a4i@gmail.com>
2026-04-30 03:31:24 +0800

    shiksha-api: allow specifying any model name for chat completion

--- shiksha-api/app-service/app/config.py
+++ shiksha-api/app-service/app/config.py
@@ -1,4 +1,3 @@
-from openai.types import ResponsesModel
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
@@ -21,7 +20,7 @@ class Settings(BaseSettings):
     azure_openai_api_key: Optional[str] = None
     azure_openai_endpoint: Optional[str] = None
     azure_openai_api_version: Optional[str] = None
-    azure_openai_deployment_name: Optional[ResponsesModel] = None
+    azure_openai_deployment_name: Optional[str] = None
     azure_openai_embed_model: Optional[str] = None
     azure_chat_deployment_name : Optional[str] = None
 
