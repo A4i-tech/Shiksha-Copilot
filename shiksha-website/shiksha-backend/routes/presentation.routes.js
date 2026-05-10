06d86302a3b0889377b2639d15703f503775f8dd
Muqsit <notmuqsit+a4i@gmail.com>
2026-05-10 23:24:24 +0800

    shiksha-backend: proxy shiksha-api /presentation

---
+++ shiksha-website/shiksha-backend/routes/presentation.routes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const { isAuthenticated } = require("../middlewares/auth.js");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const createPresentationProxy = target => createProxyMiddleware({
+	target,
+	changeOrigin: true,
+	selfHandleResponse: false,
+	proxyTimeout: 0,
+	timeout: 0,
+	on: {
+		proxyReq: (proxyReq, req) => {
+			if (req.user?._id) {
+				proxyReq.setHeader("X-User-ID", String(req.user._id));
+			}
+		},
+		proxyRes: (proxyRes) => {
+			proxyRes.headers["x-accel-buffering"] = "no";
+			proxyRes.headers["cache-control"] = "no-cache";
+		}
+	}
+});
+
+router.use("/presentation/events", createPresentationProxy(process.env.LLM_API_BASE_URL + "/presentation/events"));
+router.use(
+	"/presentation",
+	isAuthenticated,
+	createPresentationProxy(process.env.LLM_API_BASE_URL + "/presentation")
+);
+
+module.exports = router;
