5a314d630b69a235f978bfb130ba5d2482a24b7b
Muqsit <notmuqsit+a4i@gmail.com>
2026-06-12 15:06:32 +0800

    pptx-gen: run libmagic ops in child thread

--- shiksha-api/app-service/app/services/presentation/utils.py
+++ shiksha-api/app-service/app/services/presentation/utils.py
@@ -190,7 +190,7 @@ async def save_file_with_hash(storage: Storage, file: UploadFile, filename: str,
         await f.flush()
         await f.seek(0)
         path = pathlib.Path(str(f.name))
-        mime = magic.from_file(path, mime=True)
+        mime = await asyncio.to_thread(magic.from_file, path, mime=True)
         if mime not in allowed_mimes: raise ValueError("Unexpected mime (%s)" % mime)
 
         final_name = f"{sha256.hexdigest()}{suffix}"
