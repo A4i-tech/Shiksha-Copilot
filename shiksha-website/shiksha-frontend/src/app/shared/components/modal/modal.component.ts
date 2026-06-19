aad8e9dbf135b0e5835dfc337275a260cf3201b9
Punit Kaushik <122111594+Pun-it@users.noreply.github.com>
2026-06-19 20:15:48 +0530

    Revert "a11y group 6 issues resolved (#91)" (#97)
    This reverts commit 164b2dd8d651acc5976adac8205df288c4dfe525.

--- shiksha-website/shiksha-frontend/src/app/shared/components/modal/modal.component.ts
+++ shiksha-website/shiksha-frontend/src/app/shared/components/modal/modal.component.ts
@@ -1,34 +1,17 @@
-import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { ModalService } from './modal.service';
 import { CommonModule } from '@angular/common';
-import { A11yModule } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
   standalone:true,
-  imports:[CommonModule, A11yModule]
+  imports:[CommonModule]
 })
-export class ModalComponent implements OnInit, OnDestroy {
-  private previousActiveElement: HTMLElement | null = null;
+export class ModalComponent{
 
   constructor(private modalService: ModalService){}
-
-  ngOnInit() {
-    this.previousActiveElement = document.activeElement as HTMLElement;
-  }
-
-  ngOnDestroy() {
-    if (this.previousActiveElement) {
-      this.previousActiveElement.focus();
-    }
-  }
-
-  @HostListener('document:keydown.escape', ['$event'])
-  handleEscape(event: KeyboardEvent) {
-    this.closeModal();
-  }
 
   closeModal(){
     this.modalService.showDeleteUserDialog = false;
