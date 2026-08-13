4089d236de4d6a4b6ab4548d5f4b878279d8c8bd
Muqsit <notmuqsit+a4i@gmail.com>
2026-08-14 04:21:44 +0800

    teacher.absent: require training.view perm

--- shiksha-website/shiksha-backend/routes/teacher.absent.routes.js
+++ shiksha-website/shiksha-backend/routes/teacher.absent.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const teacherAbsentController = require('../controllers/teacher.absent.controller');
-const { isAuthenticated } = require('../middlewares/auth');
+const { isAuthenticated, requirePermission } = require('../middlewares/auth');
 const asyncMiddleware = require('../middlewares/asyncMiddleware.js');
 
-router.get('/teacher-absent', isAuthenticated, asyncMiddleware(teacherAbsentController.getAllAbsentTeachers));
-router.get('/teacher-absent/batch/:batchId', isAuthenticated, asyncMiddleware(teacherAbsentController.getAbsentTeachersByBatch));
+router.get('/teacher-absent', isAuthenticated, requirePermission('training.view'), asyncMiddleware(teacherAbsentController.getAllAbsentTeachers));
+router.get('/teacher-absent/batch/:batchId', isAuthenticated, requirePermission('training.view'), asyncMiddleware(teacherAbsentController.getAbsentTeachersByBatch));
 
-module.exports = router; 
+module.exports = router;
