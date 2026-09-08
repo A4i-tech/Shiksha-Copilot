0b9c088b3fb1ec7dfb37a7148fac9653faa8a78d
N1kunj1998 <njkhakhkhar@gmail.com>
2026-09-08 14:37:46 +0530

    chore(dev): set local Superset URLs and dashboard UUIDs in dev env

--- shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
+++ shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
-    apiUrl: 'your_backend_url',
+    apiUrl: 'http://localhost:8080/api',
     CRYPTO_SECRET: 'your_crypto_secret',
     EXP_MONTH: 3,
     turnstileSiteKey: '',
-    supersetUrl: 'your_superset_url',
-    supersetDashboardUuid: 'your_superset_dashboard_uuid',
-    supersetMobileDashboardUuid: 'your_superset_mobile_dashboard_uuid',
+    supersetUrl: 'http://localhost:8088',
+    supersetDashboardUuid: '31c011ec-6da5-44b1-9d66-c13fe7f1ba35',
+    supersetMobileDashboardUuid: '614ec39c-fc98-437b-966c-050c7ce47815',
 };
