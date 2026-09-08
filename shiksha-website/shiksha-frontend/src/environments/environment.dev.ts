3df17ecd28f60c773c465c580968e0db9ea376bd
N1kunj1998 <njkhakhkhar@gmail.com>
2026-09-08 14:38:38 +0530

    Revert "chore(dev): set local Superset URLs and dashboard UUIDs in dev env"
    This reverts commit 8b72a09b45432668fbe5f313bd3cf2de5206156a.

--- shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
+++ shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'your_backend_url',
     CRYPTO_SECRET: 'your_crypto_secret',
     EXP_MONTH: 3,
     turnstileSiteKey: '',
-    supersetUrl: 'http://localhost:8088',
-    supersetDashboardUuid: '31c011ec-6da5-44b1-9d66-c13fe7f1ba35',
-    supersetMobileDashboardUuid: '614ec39c-fc98-437b-966c-050c7ce47815',
+    supersetUrl: 'your_superset_url',
+    supersetDashboardUuid: 'your_superset_dashboard_uuid',
+    supersetMobileDashboardUuid: 'your_superset_mobile_dashboard_uuid',
 };
