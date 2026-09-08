fdd58352f0fc868709927274be80b4ba2829be25
N1kunj1998 <njkhakhkhar@gmail.com>
2026-09-08 16:04:58 +0530

    chore(env): remove supersetDashboardUuid from dev environment template

--- shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
+++ shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
@@ -5,6 +5,4 @@ export const environment = {
     EXP_MONTH: 3,
     turnstileSiteKey: '',
     supersetUrl: 'your_superset_url',
-    supersetDashboardUuid: 'your_superset_dashboard_uuid',
-    supersetMobileDashboardUuid: 'your_superset_mobile_dashboard_uuid',
 };
