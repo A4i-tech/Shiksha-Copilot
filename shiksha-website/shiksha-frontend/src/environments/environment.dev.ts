15b637bb3012f2985c66772b7f2820deb374ad7d
N1kunj1998 <njkhakhkhar@gmail.com>
2026-09-22 11:54:45 +0530

    fix(ci): move runtimeEnv spread before explicit fields to fix TS2783 duplicate key error; add leaflet deps and regenerate package-lock

--- shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
+++ shiksha-website/shiksha-frontend/src/environments/environment.dev.ts
@@ -1,10 +1,10 @@
 import { runtimeEnv } from "./runtime-env";
 
 export const environment = {
+    ...runtimeEnv,
     production: false,
     apiUrl: "your_backend_url",
     CRYPTO_SECRET: "your_crypto_secret",
     EXP_MONTH: 3,
     turnstileSiteKey: "",
-    ...runtimeEnv,
 };
--- shiksha-website/shiksha-frontend/src/environments/environment.prod.ts
+++ shiksha-website/shiksha-frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 import { runtimeEnv } from "./runtime-env";
 
 export const environment = {
+    ...runtimeEnv,
     production: true,
     CRYPTO_SECRET: "your_crypto_secret",
     EXP_MONTH: 3,
     turnstileSiteKey: "your_turnstile_site_key",
-    ...runtimeEnv,
 };
